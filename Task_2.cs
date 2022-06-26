422b01a9481d4525d23871628f138c77f7eaa662
pavellyub <107138984+pavellyub@users.noreply.github.com>
2022-06-26 18:13:37 +0500

    Second task

---
+++ Task_2.cs
@@ -0,0 +1,18 @@
+﻿/* Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+Console.WriteLine("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+int result = 0;
+int advance = 0;
+for(int i = 1; i < A; i++)
+{
+    advance = A % 10;
+    result = result + advance;
+    A = A / 10;
+}
+
+Console.WriteLine($"Ответ: {result}");
