24cf275e7f0e85f0d447d4d48b50ba20333f731b
pavellyub <107138984+pavellyub@users.noreply.github.com>
2022-06-28 20:49:28 +0500

    Third task

---
+++ Task_3.cs
@@ -0,0 +1,20 @@
+﻿/* Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Вывод сделать отдельным методом.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19] 
+6, 1, 33 -> [6, 1, 33] */
+
+int[] numbers = new int [8];
+
+for(int i = 1; i < numbers.Length; i++)
+{
+    numbers[i] = new Random().Next(0, 1000);
+}
+
+Printer(numbers);
+
+void Printer (int[] numbers)
+{
+    for(int i = 1; i < numbers.Length; i++)
+    {
+        Console.WriteLine(numbers[i]);
+    }
+}
